81859f3fd3304a2d6f4bf9b9b2b145a92f355ef5
Sarthak Arora <sarthakk.arora1@gmail.com>
2025-03-19 23:34:24 +0530

    Add files via upload

---
+++ Maximum and Minimum in an Array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void findMinMax(int arr[], int n) {
+    int minVal = arr[0], maxVal = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        minVal = min(minVal, arr[i]);
+        maxVal = max(maxVal, arr[i]);
+    }
+
+    cout << "Min: " << minVal << ", Max: " << maxVal;
+}
+
+int main() {
+    int arr[] = {3, 1, 5, 9, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    findMinMax(arr, n);
+    return 0;
+}
