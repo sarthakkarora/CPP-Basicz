0e27c4383e79620412f8141349f373c52d9941ff
Sarthak Arora <sarthakk.arora1@gmail.com>
2025-03-31 20:18:18 +0530

    Add files via upload

---
+++ Max Subarray.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int maxSubArray(std::vector<int>& nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+        
+        for (int i = 1; i < nums.size(); i++) {
+            currentSum = std::max(nums[i], currentSum + nums[i]);
+            maxSum = std::max(maxSum, currentSum);
+        }
+        return maxSum;
+    }
+};
