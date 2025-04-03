d8837af5da21b2351e2160ee36fb0c4bbb165df0
Sarthak Arora <sarthakk.arora1@gmail.com>
2025-04-03 22:41:44 +0530

    Add files via upload

---
+++ Validate Bst.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+    public:
+        bool isValidBST(TreeNode* root) {
+            TreeNode* prev = nullptr;
+            stack<TreeNode*> st;
+            while (root || !st.empty()) {
+                while (root) {
+                    st.push(root);
+                    root = root->left;
+                }
+                root = st.top();
+                st.pop();
+                if (prev && root->val <= prev->val) return false;
+                prev = root;
+                root = root->right;
+            }
+            return true;
+        }
+    };
+    
+    
