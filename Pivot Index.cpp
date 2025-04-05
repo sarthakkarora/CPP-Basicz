54056906e08940234a76a2b390b9c92904252748
Sarthak Arora <sarthakk.arora1@gmail.com>
2025-04-05 23:36:13 +0530

    Add files via upload

---
+++ Pivot Index.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <numeric> 
+
+class Solution {
+public:
+    int pivotIndex(std::vector<int>& nums) {
+        int totalSum = std::accumulate(nums.begin(), nums.end(), 0);
+        int leftSum = 0;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        
+        return -1;
+    }
+};
