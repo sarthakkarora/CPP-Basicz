16727f1b4e58a85ef4def9ee0bc55ff8bda1a339
Sarthak Arora <sarthakk.arora1@gmail.com>
2025-05-02 23:37:30 +0530

    Update Climbing Stairs.cpp

--- Climbing Stairs.cpp
+++ Climbing Stairs.cpp
@@ -2,7 +2,6 @@ class Solution {
     public:
         int climbStairs(int n) {
             if (n == 1) return 1;
-            
             int first = 1, second = 2;
             for (int i = 3; i <= n; i++) {
                 int third = first + second;
