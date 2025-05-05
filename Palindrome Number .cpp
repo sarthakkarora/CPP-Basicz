7970277e7c90905e89b9b6d0f82362d48ac1e41d
Sarthak Arora <sarthakk.arora1@gmail.com>
2025-05-05 23:08:31 +0530

    Update Palindrome Number .cpp

--- Palindrome Number .cpp
+++ Palindrome Number .cpp
@@ -4,7 +4,6 @@ class Solution {
             if (x < 0 || (x % 10 == 0 && x != 0)) {
                 return false;
             }
-    
             int reversed = 0;
             while (x > reversed) {
                 reversed = reversed * 10 + x % 10;
