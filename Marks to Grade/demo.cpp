94fc3c420b0cc873ca1b03291848f0b5698440ab
Sarthak Arora <sarthakk.arora1@gmail.com>
2025-07-02 11:33:41 +0530

    Add files via upload

---
+++ Marks to Grade/demo.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int marks;
+  cin >> marks;
+  if(marks < 0 || marks > 100) {
+    cout << "Enter valid marks i.e. between (0-100)";
+  }
+  else if(marks < 25){
+    cout << "Grade: F";
+  }
+  else if (marks <= 44) {
+    cout << "Grade: E";
+  }
+  else if (marks <= 49) {
+    cout << "Grade: D";
+  }
+  else if (marks <= 59) {
+    cout << "Grade: C";
+  }
+  else if (marks <= 79) {
+    cout << "Grade: B";
+  }
+  else {
+    cout << "Grade: A";
+  }
+
+  return 0;
+}
